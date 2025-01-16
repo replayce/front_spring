8698e29d2f960c52794133e1b9f76c7988a5b96b
ByeongJinOh <diagod4806@gmail.com>
2025-01-16 13:51:42 +0900

    - Admin 관련 assets 추가 - Admin controller 추가

---
+++ src/main/java/com/replayce/front/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.replayce.front.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping
+    public String mainPage(Model model) {
+//        model.addAttribute("sample", "Replayce");
+        return "admin/admin_main";
+    }
+}
