51b07d27d25706bfe15982712b1b19156103adc1
ohhyungu <ohhyungu93@gmail.com>
2025-01-17 15:07:35 +0900

    로그인 회원가입 페이지 완성, 사이드바에 페이지 링크 연결

--- src/main/java/com/replayce/front/controller/AdminController.java
+++ src/main/java/com/replayce/front/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.replayce.front.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +9,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/admin")
+//@RequestMapping("/admin")
+@Slf4j
 public class AdminController {
     @GetMapping
     public String mainPage(Model model) {
 //        model.addAttribute("sample", "Replayce");
         return "admin/admin_login";
     }
+    @GetMapping("/admin")
+    public String main() {
+        return "admin/admin_main";
+    }
     @GetMapping("/admin_login")
-    public String index() {
+    public String login() {
         return "admin/admin_login";
     }
+    @GetMapping("/admin_signup")
+    public String signup() {
+        return "admin/admin_signup";
+    }
+    @GetMapping("/admin_reports")
+    public String reports() {
+        return "admin/admin_reports";
+    }
+    @GetMapping("/admin_alerts")
+    public String alerts() {
+        return "admin/admin_alerts";
+    }
+    @GetMapping("/admin_setting")
+    public String setting() {
+        return "admin/admin_setting";
+    }
+
+
 }
