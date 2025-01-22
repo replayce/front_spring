6814af33932fc57b47ade53f35aa64b115275ae2
ByeongJinOh <diagod4806@gmail.com>
2025-01-22 22:41:36 +0900

    - File 업로드 사이즈 제한 10MB로 변경 - Image File 외에는 에러 처리 진행

--- src/main/java/com/replayce/front/controller/FileController.java
+++ src/main/java/com/replayce/front/controller/FileController.java
@@ -6,22 +6,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/upload")
-public class fileController {
+public class FileController {
 
     private final JavaClient javaClient;
 
     @PostMapping
     public ResponseEntity<CommonResponse<String>> uploadFile(HttpServletRequest request, @RequestParam("file") MultipartFile file) {
-        CommonResponse<String> res = javaClient.uploadFile(file);
-
-        return new ResponseEntity<>(res, HttpStatus.OK);
+        try {
+            CommonResponse<String> res = javaClient.uploadFile(file);
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        } catch (Exception e) {
+            String errMsg = e.getMessage();
+            if (errMsg.contains("400")) {
+                return new ResponseEntity<>(new CommonResponse<String>(request.getRequestURI(),
+                        "Only image files can be uploaded.",
+                        "",
+                        null), HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
