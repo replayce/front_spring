210b6a360e6a867a47f150a6097bf54ffea5c3ed
wlsl10 <ginny1002@naver.com>
2025-01-22 11:08:34 +0900

    예진-프론트 코드 작성

--- src/main/java/com/replayce/front/client/api/BoardClient.java
+++ src/main/java/com/replayce/front/client/api/BoardClient.java
@@ -1,4 +1,33 @@
 package com.replayce.front.client.api;
 
+import com.replayce.front.client.dto.BaseResponse;
+import com.replayce.front.client.dto.Board;
+import com.replayce.front.client.dto.BoardResponse;
+import com.replayce.front.client.dto.CommonResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "boardClient", url = "http://localhost:8081/api")
 public interface BoardClient {
+
+    @GetMapping("/board")
+    CommonResponse<List<BoardResponse>> getAllBoards();
+
+    @GetMapping("/board/{boardId}")
+    CommonResponse<BoardResponse> getBoardById(@PathVariable Long boardId);
+
+    @PostMapping("/board")
+    CommonResponse<BoardResponse> createBoard(@RequestBody Board board);
+
+    @PatchMapping("/board/{boardId}")
+    CommonResponse<BoardResponse> updateBoard(
+            @PathVariable Long boardId,
+            @RequestBody Board boardDetails
+    );
+
+    @DeleteMapping("/board/{boardId}")
+    ResponseEntity<BaseResponse> deleteBoard(@PathVariable Long boardId);
 }
--- src/main/java/com/replayce/front/client/dto/BoardResponse.java
+++ src/main/java/com/replayce/front/client/dto/BoardResponse.java
@@ -1,4 +1,18 @@
 package com.replayce.front.client.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//API 응답 데이터 DTO //서버->클라이언트
 public class BoardResponse {
+    private String writer;
+    private String writerNumber;
+    private String writerPassword;
+    private String content;
+    private String imageUrl;
+    private String date;
+    private String hour;
+    private String minute;
 }
