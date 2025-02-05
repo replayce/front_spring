a169df3a380d7232d8b3263712698a982deff770
yyoungm <1971103@ewhain.net>
2025-02-05 11:35:44 +0900

    어드민 제보관리 페이지
    - 제보 리스트 업데이트
    - 클릭시 제보 페이지 이동

--- src/main/java/com/replayce/front/client/dto/ReportResponse.java
+++ src/main/java/com/replayce/front/client/dto/ReportResponse.java
@@ -3,17 +3,33 @@ package com.replayce.front.client.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
 public class ReportResponse {
     private int id;
     private String writer;
+    private LocalDateTime createDate;
     private String writerNumber;
     private String date;
     private int hour;
     private int minute;
     private String location;
     private String jelly;
+
+    // 작성일시 포맷 수정
+    private String formattedCreateDate;
+
+    public String getFormattedCreateDate() {
+        if(createDate == null) {
+            return "작성일시 없음";
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return createDate.format(formatter);
+    }
+
+
 }
